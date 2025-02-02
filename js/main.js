51e293c383e792455c746d55dd29b40619d989d3
Julián Potes Villada <julianpotesvillada@gmail.com>
2025-02-02 11:19:54 -0500

    Se agregan las tarjetas de la cateleras y el codigo correspindiente al pie de pagina

--- js/main.js
+++ js/main.js
@@ -5,4 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(data =>{
                 document.getElementById("menu-container").innerHTML = data;
             })
-});
+    // carga el footer en las paginas
+        fetch('footer.html')
+            .then(response => response.text())
+            .then(data =>{
+                document.getElementById("cotenedor-footer").innerHTML = data;
+            })
+    });
