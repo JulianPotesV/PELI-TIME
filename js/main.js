e0913eb0013664bd290bd632343b999030bd5880
Julián Potes Villada <julianpotesvillada@gmail.com>
2025-02-17 08:28:18 -0500

    Se intento obtener datos de la base de datos, se obtuvieron pero al parecer no de la manera correcta

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     // carga el menu en las paginas
-        fetch('menu.html')
-            .then(response => response.text())
-            .then(data =>{
-                document.getElementById("menu-container").innerHTML = data;
-            })
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("menu-container").innerHTML = data;
+        });
+
     // carga el footer en las paginas
-        fetch('footer.html')
-            .then(response => response.text())
-            .then(data =>{
-                document.getElementById("cotenedor-footer").innerHTML = data;
-            })
-    });
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("cotenedor-footer").innerHTML = data;
+        });
+    
+
+});
+
+document.addEventListener('DOMContentLoaded',async () => {
+        const tituloPelicula = await obtenerDatoPelicula(1, "titulo");
+        if (tituloPelicula) {
+            const elemento = document.getElementById("titulo-pelicula-1");
+            elemento.textContent = tituloPelicula;
+        } else {
+            console.log("No se encontro la pelicula");
+        }
+});
--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
-// Importar módulos
-const express = require('express');
-const { Pool } = require('pg');
+import {pool} from "../db/conectionPostgresSQL.js";
 
-// Configurar el servidor Express
-const app = express();
-const port = 3000;
+const obtenerDatoPelicula = async (idpeliculas, propiedad) => {
+    try {
+        const resultado = await pool.query("SELECT " + propiedad + " FROM peliculas WHERE idpeliculas = " + idpeliculas);
+        return resultado.rows[0][propiedad]
+    } catch (error) {
+        console.log(error);
+        return null;
+    }   
+};
 
-// Configurar la conexión a la base de datos PostgreSQL
-const pool = new Pool({
-    user: 'postgres',       
-    host: 'localhost',      
-    database: 'pelitime_bd',
-    password: '123456',     
-});
+const agregarPelicula = async ()=>{
+    try {
+        const resultado = await pool.query("INSERT INTO peliculas (idpeliculas, titulo, descripcion, duracion, clasificacion, genero, director, actores)"
+                                                                     + "VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", [2, "Mohana 2", "Moana 2 sigue a Moana tres años después de su primera aventura. Guiada por una misteriosa llamada ancestral, se embarca en un nuevo viaje por los mares desconocidos junto a Maui y una inesperada tripulación para proteger a su pueblo.", 100, "todo publico", "animada", "David G. Derrick Jr", "Auliʻi Cravalho, Dwayne 'La Roca' Johnson" ]);
+        console.log(resultado);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+obtenerDatoPelicula(1, "titulo");
